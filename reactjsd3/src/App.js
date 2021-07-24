377c204d205dd772a7874edc34f1df534f19d210
xing cong <121950263@qq.com>
2021-07-25 02:03:47 +0800

    update

--- reactjsd3/src/App.js
+++ reactjsd3/src/App.js
@@ -1,13 +1,40 @@
  
-import './App.css';
-import Home from './views/Home.js'
 
-function App() {
+
+import './App.css';
+import React from "react";
+
+import Home from "./views/Home.js"
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-        <Home />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/Home"> Home </Link>
+            </li>
+           
+          
+          </ul>
+        </nav>
+ 
+        <Switch>
+        
+          <Route path="/Home">
+            <Home />
+          </Route>
+        
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
