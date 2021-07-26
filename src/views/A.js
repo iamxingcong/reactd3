8966aab0c4d886b73c1c468004f2b810dacbcd39
v_coxing <v_coxing@tencent.com>
2021-07-26 18:43:40 +0800

    up

--- src/views/A.js
+++ src/views/A.js
@@ -18,71 +18,71 @@ class A extends React.Component {
       //     throw err;
       // })   
 
-        var width = window.screen.width
-        var height = 900
-        var svg = d3
-        .select(".c")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height)
+              //JSON object with the data
 
-       
-        var   g = svg.append("g").attr("transform", "translate(" + (width / 2 - 15) + "," + (height / 2 + 25) + ")");
-
-          var stratify = d3.stratify()
-          .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
-
-          var tree = d3.cluster()
-          .size([360, 390])
-          .separation(function(a, b) { return (a.parent === b.parent ? 1 : 2) / a.depth; });
-
-          d3.csv(data).then(function(data) {
-
+              console.log(data)
+              var width = 960
+              var height = 3200
+              var svg = d3
+              .select(".c")
+              .append("svg")
+              .attr("width", width)
+              .attr("height", height)
+      
             
-          var root = tree(stratify(data)
-            .sort(function(a, b) { return (a.height - b.height) || a.id.localeCompare(b.id); }));
+            // var svg = d3.select("svg"),
+            // width = +svg.attr("width"),
+            // height = +svg.attr("height"),
+        var   g = svg.append("g").attr("transform", "translate(40,0)");
 
-          var link = g.selectAll(".link")
-          .data(root.descendants().slice(1))
-          .enter().append("path")
-            .attr("class", "link")
-            .attr("d", function(d) {
-              return "M" + project(d.x, d.y)
-                  + "C" + project(d.x, (d.y + d.parent.y) / 2)
-                  + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
-                  + " " + project(d.parent.x, d.parent.y);
-            });
+            var tree = d3.cluster()
+            .size([height, width - 160]);
 
-          console.log(link)
-          
-          var node = g.selectAll(".node")
-          .data(root.descendants())
-          .enter().append("g")
-            .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-            .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; });
+            var stratify = d3.stratify()
+            .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
 
-          node.append("circle")
-            .attr("r", 2.5);
+         d3.csv(data).then(function(data) {
+            var root = stratify(data)
+              .sort(function(a, b) { return (a.height - b.height) || a.id.localeCompare(b.id); });
 
-          node.append("text")
-            .attr("dy", ".21em")
-            .attr("x", function(d) { return d.x < 180 === !d.children ? 6 : -6; })
-            .style("text-anchor", function(d) { return d.x < 180 === !d.children ? "start" : "end"; })
-            .attr("transform", function(d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; })
-            .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
-          });
+            tree(root);
 
-          function project(x, y) {
-          var angle = (x - 90) / 180 * Math.PI, radius = y;
-          return [radius * Math.cos(angle), radius * Math.sin(angle)];
-          }
+            var link = g.selectAll(".link")
+              .data(root.descendants().slice(1))
+            .enter().append("path")
+              .attr("class", "link")
+              .attr("d", function(d) {
+                return "M" + d.y + "," + d.x
+                    + "C" + (d.parent.y + 100) + "," + d.x
+                    + " " + (d.parent.y + 100) + "," + d.parent.x
+                    + " " + d.parent.y + "," + d.parent.x;
+              });
 
-  }
+              console.log(link)
+
+            var node = g.selectAll(".node")
+              .data(root.descendants())
+            .enter().append("g")
+              .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+              .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+
+            node.append("circle")
+              .attr("r", 2.5);
+
+            node.append("text")
+              .attr("dy", 3)
+              .attr("x", function(d) { return d.children ? -8 : 8; })
+              .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+              .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
+            
+              }
+
+        )}
     
     render() {
         return (
               <div className="c" > 
-              
+               
               </div>
           )
     }
